9b41f4ee49e8fafa66b2fc80e6c4f7715056479e
António Santos <antoniojps@ua.pt>
2020-03-23 15:02:16 +0000

    Add exports Card and Search

--- components/molecules/index.js
+++ components/molecules/index.js
@@ -3,3 +3,5 @@ export { default as InputMultiple } from './Input/InputMultiple';
 export { default as InputText } from './Input/InputText';
 export { default as InputDropdown } from './Input/InputDropdown';
 export { default as InputPassword } from './Input/InputPassword';
+export { default as Card } from './Card/Card';
+export { default as Search } from './Search/Search';
