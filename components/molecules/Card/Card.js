8d1acc5610db4568ea870bdab615959c36fe0e0a
António Santos <antoniojps@ua.pt>
2020-03-23 15:02:51 +0000

    Rework Card props to match graphql schema

--- components/molecules/Card/Card.js
+++ components/molecules/Card/Card.js
@@ -1,33 +1,28 @@
 import React from 'react';
-import { faEllipsisV, faMapMarkerAlt, faClock } from '@fortawesome/free-solid-svg-icons'
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
 import { PersonHeader, Button, Label, Icon } from 'components/atoms';
 import './Card.module.scss';
 
-const Card = ({ name, desc, src, competences, location, availability }) => {
+const Card = ({ name, job, src = 'https://scontent.fbru2-1.fna.fbcdn.net/v/t1.0-9/p960x960/83684971_10219246916195136_7758163107968450560_o.jpg?_nc_cat=107&_nc_sid=85a577&_nc_ohc=dT0bqq6Uj5YAX-zE6aO&_nc_ht=scontent.fbru2-1.fna&_nc_tp=6&oh=60ae1502736c6c0dfc6f7d0cd642264d&oe=5E9C0CE9', categories, locations }) => {
     return (
         <div className='card'>
             <div className='card__top'>
-                <PersonHeader name={name} desc={desc} src={src} />
-                <Icon icon={faEllipsisV} />
+                <PersonHeader name={name} desc={job} src={src} />
             </div>
 
             <div className='card__body'>
                 <div className='card__body__competences'>
                     <p>Competências</p>
                     <div className='card__body__competences__list'>
-                        {competences.map(item => <Label key={item.id} text={item.label} />)}
+                        {categories.map(item => <Label key={item._id} text={item.name} />)}
                     </div>
                 </div>
-
-                <div className='card__body__item'>
-                    <Icon icon={faMapMarkerAlt} />
-                    <p>{location.location}</p>
-                </div>
-
-                <div className='card__body__item'>
-                    <Icon icon={faClock} />
-                    <p>{availability}h<span>/semana</span></p>
-                </div>
+                {locations.map(location => (
+                    <div className='card__body__item' key={location._id}>
+                        <Icon icon={faMapMarkerAlt} />
+                        <p>{location.name}</p>
+                    </div>
+                ))}
             </div>
 
             <div className='card__footer'>
--- components/molecules/Card/Card.stories.js
+++ components/molecules/Card/Card.stories.js
@@ -8,24 +8,21 @@ export default {
 
 const props = {
   name: 'Henrique Silva',
-  desc: 'Enfermeiro',
+  job: 'Enfermeiro',
   src: 'https://scontent.fbru2-1.fna.fbcdn.net/v/t1.0-9/p960x960/83684971_10219246916195136_7758163107968450560_o.jpg?_nc_cat=107&_nc_sid=85a577&_nc_ohc=dT0bqq6Uj5YAX-zE6aO&_nc_ht=scontent.fbru2-1.fna&_nc_tp=6&oh=60ae1502736c6c0dfc6f7d0cd642264d&oe=5E9C0CE9',
-  location: {
-    geo: {
-      long: '',
-      lat: '',
-    },
-    location: 'Aveiro',
-  },
-  availability: 20,
-  competences: [
+  locations: [
     {
-      id: 0,
-      label: 'Saude',
+      name: 'Aveiro',
+    },
+  ],
+  categories: [
+    {
+      _id: 0,
+      name: 'Saude',
     },
     {
-      id: 1,
-      label: 'Apoio Social',
+      _id: 1,
+      name: 'Apoio Social',
     },
   ],
 }
