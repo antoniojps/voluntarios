dc53044707de5fad2fecb7896ea010b72fc091ce
António Santos <antoniojps@ua.pt>
2020-03-25 18:58:35 +0000

    Polish Card anim

--- components/molecules/Card/Card.js
+++ components/molecules/Card/Card.js
@@ -62,7 +62,7 @@ const Card = memo(({ name, job, src = null, categories, locations, loading }) =>
                         initial={{ opacity: 0 }}
                         animate={{ opacity: 1 }}
                         exit={{ opacity: 0 }}
-                        transition={{ duration: 1 }}
+                        transition={{ duration: 0.2 }}
                         key="loading"
                     >
                         {renderLoading()}
@@ -72,7 +72,7 @@ const Card = memo(({ name, job, src = null, categories, locations, loading }) =>
                         initial={{ opacity: 0 }}
                         animate={{ opacity: 1 }}
                         exit={{ opacity: 0 }}
-                        transition={{ duration: 1 }}
+                        transition={{ duration: 0.2 }}
                         key="default"
                     >
                         {renderDefault()}
