a38c9140a5afbb779acb25f8f72c583df72501b0
António Santos <antoniojps@ua.pt>
2020-03-25 14:59:53 +0000

    Polish Card

--- components/molecules/Card/Card.js
+++ components/molecules/Card/Card.js
@@ -6,26 +6,26 @@ import './Card.module.scss';
 const Card = ({ name, job, src = null, categories, locations }) => {
     return (
         <div className='card'>
-            <div className='card__top'>
-                <PersonHeader name={name} desc={job} src={src} />
-            </div>
-
-            <div className='card__body'>
-                <div className='card__body__competences'>
-                    <p>Competências</p>
-                    <div className='card__body__competences__list'>
-                        {categories.map(item => <Label key={item._id} text={item.name} background={item.color}/>)}
+            <div className="card__top">
+                <div className='card__header'>
+                    <PersonHeader name={name} desc={job} src={src} />
+                </div>
+                <div className='card__body'>
+                    <div className='card__body__competences'>
+                        <p>Competências</p>
+                        <div className='card__body__competences__list'>
+                            {categories.map(item => <Label key={item._id} text={item.name} background={item.color}/>)}
+                        </div>
                     </div>
                 </div>
+            </div>
+            <div className="card__bottom">
                 {locations.map(location => (
                     <div className='card__body__item' key={location._id}>
                         <Icon icon={faMapMarkerAlt} />
                         <p>{location.name}</p>
                     </div>
                 ))}
-            </div>
-
-            <div className='card__footer'>
                 <Button onClick={console.log} type='secondary'>
                     contactar
                 </Button>
