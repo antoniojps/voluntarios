212950c731974bb5e5d96f28d2ea026e933de67b
António Santos <antoniojps@ua.pt>
2020-03-27 18:15:03 +0000

    Add secureUserOnly function and jsdoc for secure and secureUserOnly functions

--- apollo/utils/filters.js
+++ apollo/utils/filters.js
@@ -5,9 +5,16 @@ import {
 } from 'apollo-server-micro';
 import status from 'http-status'
 
+/**
+ * Secures a resolver for authenticated users only
+ * @param {Function} func Resolver
+ * @param {Boolean} admin Must be admin to call this resolver?
+ * @param {Boolean} moderator Must be moderator to call this resolver?
+ * @returns {Function} Resolver secured
+ */
 export function secure(func, admin = false, moderator = false) {
   return async (root, args, context) => {
-    let { user } = context.req;
+    const { user } = context.req;
     if (!user) throw new AuthenticationError('Unauthenticated');
     // admin only
     if (admin && !moderator && !user.admin) {
@@ -19,6 +26,28 @@ export function secure(func, admin = false, moderator = false) {
     return func(root, args, { ...context, user });
   };
 }
+
+/**
+ * Secures a resolver for a specific user (unless admin or moderator)
+ * @param {Function} func Resolver
+ * @param {Boolean} userIdParam userId input parameter key "id","userId"
+ * @returns {Function} Resolver secured
+ */
+export function secureUserOnly (func, userIdParam = 'userId') {
+  return (root, args, context) => {
+    const { user } = context.req;
+    if (!user) throw new AuthenticationError('Unauthenticated')
+    else if (
+      !user.admin &&
+      !user.moderator &&
+      args[userIdParam] !== user.id
+    ) {
+      throw new ForbiddenError('Unauthorized')
+    }
+    return func(root, args, context)
+  }
+}
+
 
 export async function paginate (Model, query, pagination, sort) {
   const { limit = 10, page = 1 } = pagination
