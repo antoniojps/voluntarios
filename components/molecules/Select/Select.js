073c3a77902a872ca3e5e3e213ab545343cad74f
António Santos <antoniojps@ua.pt>
2020-03-27 04:51:21 +0000

    Add Select with react-select

---
+++ components/molecules/Select/Select.js
@@ -0,0 +1,84 @@
+/* Custom Input Component to be used with Formik */
+import React, { useMemo } from 'react';
+import Select, { components } from 'react-select';
+import './Select.module.scss'
+
+const MultiValueContainer = props => {
+  return (
+    <div className="multi-value">
+      <components.MultiValueContainer {...props} />
+      <style jsx>{`
+        .multi-value {
+            margin: var(--spacing-xs5) var(--spacing-xs4) var(--spacing-xs5) 0;
+            border-radius: var(--borderRadius);
+            background-color: ${props.data.color || 'black'} !important;
+        }
+      `}</style>
+    </div>
+  );
+};
+
+
+const FieldSelect = ({
+  options,
+  creatable,
+  isMulti,
+  name,
+  onBlur,
+  onChange,
+  value,
+  placeholder = 'Seleccione',
+  ...props
+}) => {
+
+  const handleChange = option => {
+    if (isMulti) {
+      const values = option ? option.map(({ value }) => value) : [];
+      onChange(values)
+      return;
+    }
+    if (option && option.value) onChange(option.value);
+    onChange(null)
+  };
+
+  const noOptionsMessage = useMemo(() => {
+    if (creatable) return 'Digite para criar uma opção';
+    return 'Sem opções';
+  }, [creatable]);
+
+  const isomorphicWindow = typeof window !== 'undefined' ? window : {}
+  return (
+      <Select
+        classNamePrefix="react-select"
+        noOptionsMessage={() => noOptionsMessage}
+        options={options}
+        name={name}
+        value={
+          options ? options.find(option => option.value === value) : ''
+        }
+        onChange={handleChange}
+        onBlur={onBlur}
+        isMulti={isMulti}
+        placeholder={placeholder}
+        components={{ MultiValueContainer }}
+        styles={{
+          multiValue: base => ({
+            ...base,
+            backgroundColor: 'transparent',
+            color: 'white',
+          }),
+          multiValueLabel: base => ({
+            ...base,
+            backgroundColor: 'transparent',
+            color: 'white',
+          }),
+          menuPortal: base => ({ ...base, zIndex: 9999 }),
+        }}
+        menuPortalTarget={isomorphicWindow && isomorphicWindow.document && isomorphicWindow.document.body ? isomorphicWindow.document.body : {}}
+        {...props}
+    >
+      </Select>
+  );
+};
+
+export default FieldSelect;
---
+++ components/molecules/Select/Select.stories.js
@@ -0,0 +1,67 @@
+import { withA11y } from "@storybook/addon-a11y";
+import Select from './Select'
+import React from 'react'
+
+export default {
+  title: "Molecules/Select",
+  decorators: [withA11y],
+};
+
+const options = [
+  { value: 'chocolate', label: 'Chocolate', color: 'brown' },
+  { value: 'strawberry', label: 'Strawberry', color: 'red' },
+  { value: 'vanilla', label: 'Vanilla', color: 'grey' },
+]
+
+export const Simple = () => {
+  const [value, setValue] = React.useState({ value: null })
+  React.useEffect(() => {
+    console.log(value)
+  }, [value])
+  return (
+    <div style={{ width: '200px' }}>
+       <Select options={options} onChange={setValue} value={value} isClearable />
+    </div>
+  )
+}
+
+export const Multiple = () => {
+  const [value, setValue] = React.useState('')
+  React.useEffect(() => {
+    console.log(value)
+  }, [value])
+  return (
+    <div style={{ width: '200px' }}>
+      <Select
+        options={options}
+        onChange={setValue}
+        value={value}
+        defaultValue={[{ value: 'chocolate', label: 'Chocolate', color: 'brown' }]}
+        isMulti
+        isClearable
+        closeMenuOnSelect={false}
+      />
+    </div>
+  )
+}
+
+export const Loading = () => {
+  const [value, setValue] = React.useState('')
+  React.useEffect(() => {
+    console.log(value)
+  }, [value])
+  return (
+    <div style={{ width: '200px' }}>
+      <Select
+        options={options}
+        onChange={setValue}
+        value={value}
+        isMulti
+        isClearable
+        closeMenuOnSelect={false}
+        isLoading
+        isDisabled
+      />
+    </div>
+  )
+}
